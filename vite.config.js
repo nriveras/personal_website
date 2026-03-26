c884f74bd42bd254dbb33a4f59e87275172a4640
nriveras <nriveras.m@gmail.com>
2026-03-26 16:25:09 +0100

    Fix deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: process.env.GITHUB_ACTIONS && repoName ? `/${repoName}/` : '/',
 })
